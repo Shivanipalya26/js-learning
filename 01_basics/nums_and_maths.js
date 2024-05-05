f3f129d8717351390e2a53f72738b22db30e3898
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2024-05-05 09:36:32 +0000

    nums file added

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,40 @@
+const score = 100
+//console.log(score);
+
+const newScore = new Number(200)
+//console.log(newScore);
+
+// console.log(newScore.toString());
+// console.log(typeof(newScore.toString()));
+// console.log(typeof(newScore));
+
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+
+const otherNum = 124.892
+// console.log(otherNum.toPrecision(3))
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+
+// *************************************MATHS***************************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.3));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(2.7))
+// console.log(Math.min(2,5,1,7));
+// console.log(Math.max(2,5,1,7));
+
+console.log(Math.random());
+console.log((Math.random() * 10) +1);
+console.log(Math.floor(Math.random() * 10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
