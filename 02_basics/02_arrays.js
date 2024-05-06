0c4f3c3feafec1eb7519db6fc6aa8b5aa99012c6
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2024-05-06 20:55:30 +0000

    array2 added

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,29 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const allNewHeros = [...marvel_heros, ...dc_heros]          //spread operator
+// console.log(allNewHeros);
+
+const another_array = [1,2,3,[4,5],6,7,[8,6,[4,5]]]
+
+const real_array = another_array.flat(Infinity)
+console.log(real_array);
+
+console.log(Array.isArray("Shivani"));
+console.log(Array.from("Shivani"));
+
+console.log(Array.from({name: "Shivani"}));             //need to define whose array need to be made
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
