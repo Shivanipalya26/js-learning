165529397738ca437e96fe48330d30e5068a8ca8
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2024-05-13 19:56:25 +0000

    file added

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+const myObj = {
+    js : "Javascript", 
+    cpp : "C++", 
+    rb : "Ruby",
+    sw : "Swift"
+}
+
+for (const key in myObj) {
+    // console.log(`${key} is ${myObj[key]}`);
+}
+
+const arr = [3,4,5,6,7,8]
+
+for (const key in arr) {
+    // console.log(`${key} is ${arr[key]}`);
+}
+
+//map k upar iterations nhi kiya ja sakta
