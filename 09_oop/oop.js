d50e9cc045d3f06f4549e83be6def8727dd9e51e
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2024-05-21 19:41:51 +0000

    File added

--- 09_oop/oop.js
+++ 09_oop/oop.js
@@ -19,6 +19,10 @@ function User(username, loginCnt, isSignedIn){
     this.loginCnt = loginCnt,
     this.isSignedIn = isSignedIn
 
+    this.greeting = function() {
+        console.log(`Welcome ${this.username}`);
+    }
+
     // return this
 }
 
@@ -28,5 +32,15 @@ function User(username, loginCnt, isSignedIn){
 const user1 = new User("ABC", 5, true);
 const user2 = new User("XYZ", 15, false);
 
+//whenever new keyword is used an empty object is created which is called instance.
+//new object is created
+//constructor function is called because new keyword
+//whatever arguments are written it will be injected in this keyword
+//mil jate h function k andar
+
 console.log(user1);
 console.log(user2);
+console.log(user1.greeting());
+
+
+//read - instanceof
