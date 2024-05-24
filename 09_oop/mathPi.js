3a168189773f600718e3356bf47fd8bb66b62225
Shivani Palya <99176636+Shivanipalya26@users.noreply.github.com>
2024-05-24 19:27:51 +0000

    files added

--- 09_oop/mathPi.js
+++ 09_oop/mathPi.js
@@ -4,3 +4,30 @@ console.log(descriptor);
 // console.log(Math.PI);
 // Math.PI = 5
 // console.log(Math.PI);
+
+const chai = {
+    name: 'ginger',
+    price : 500,
+    isAvailable: true,
+
+    orderChai : function() {
+        console.log("Chai nhi banegi");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+console.log(Object.getOwnPropertyDescriptor(chai, "price"));
+
+Object.defineProperty(chai, 'name', {
+    writable: false,
+    enumerable: false
+})
+
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function') {
+        console.log(`${key} : ${value}`);
+    }
+}
